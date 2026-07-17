f21a60e090acd481e8cd5b3083b4478ef5fd4d61
Edipo Andersen <edipo.x6@gmail.com>
2026-07-17 16:43:47 -0300

    Adding script to hamburguer menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Portfolio loaded");
+const menuToggle = document.querySelector(".menu-toggle");
+
+const menu = document.querySelector("nav ul");
+
+
+menuToggle.addEventListener("click", () => {
+
+    menu.classList.toggle("active");
+
+});
